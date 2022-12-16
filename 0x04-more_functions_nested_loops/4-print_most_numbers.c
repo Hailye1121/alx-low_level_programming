25f02b105a6a80e8581b84b7e30204fb0eea35a4
Hailye1121 <www.poethm@gmail.com>
2022-12-16 15:18:58 -0800

    a function that prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,10 +2,11 @@
 
 /**
  * print_most_numbers - print num except 2 and 4
+ *
  *Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char c;
 
@@ -15,4 +16,4 @@ void print_numbers(void)
 			_putchar(c);
 	}
 	_putchar('\n');
-}	
+}
